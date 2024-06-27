5766a5b1a46105f68a304ecb03f6dd7af83000de
Salar-pr <salarrezayipr@gmail.com>
2024-06-27 20:24:58 +0330

    v1-fr.py

--- v1-fr.py
+++ v1-fr.py
@@ -1,1 +1,14 @@
+import random#فراخانی کردن کتاب خانه رندوم 
+row = 3
+column = 3
+a = 1
+b = 9
 
+
+#تعریف تابع برای ساخت جدول
+def jadval(row,column,a,b):
+    my_list=list(random.randint(a,b)for _ in range(row*column))
+
+#جدا سازی ان به مقدار ستون
+    for i in range(0, len(my_list),row):
+        print(my_list[i:i+row],"\t"*2)
